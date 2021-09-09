52ca1892f35c4f5c036a7e5f6488543aaab9f6a2
Samyak Shah <samyakshah3008@gmail.com>
2021-09-09 22:13:31 +0530

    FIX: empty and -ve value fix

--- hypotenuse.js
+++ hypotenuse.js
@@ -3,16 +3,30 @@ const calculatebutton = document.querySelector('#calculate-button')
 const output = document.querySelector('#output')
 
 function processing(a,b){
-    const sumOfSquares = a*a  + b*b
+    var sumOfSquares = a*a  + b*b
    
     return sumOfSquares
 }
 
 function calculateHypotenuse(){
     
-   const sumOfSquares= processing(Number(inputs[0].value),Number(inputs[1].value))
-    const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
-    output.innerText = "The length of hypotenuse is: " + lengthOfHypotenuse
+   var sumOfSquares= processing(Number(inputs[0].value),Number(inputs[1].value))
+    var lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+
+    if(inputs[0].value < 0){
+        output.innerText = "length cannot be negative"
+      } else if(inputs[1].value < 0){
+        output.innerText = "length cannot be negative"
+      } else if(inputs[0].value === ""){
+        output.innerText = "Fill the length box so we can check"
+     } else if(inputs[1].value === ""){
+       output.innerText = "Fill the length box so we can check"
+    }
+       else{
+        output.innerText = "The length of hypotenuse is: " + lengthOfHypotenuse
+      }
+    
+    
     
 
 }
