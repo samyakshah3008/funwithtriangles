986cc5e2e6c82fdaf27e7061104f4de3df71d519
Samyak Shah <samyakshah3008@gmail.com>
2021-09-09 22:03:03 +0530

    FIX: empty and negative value fix

--- istriangle.js
+++ istriangle.js
@@ -4,18 +4,36 @@ const output = document.querySelector('#output')
 
 function calculateSum(angle1,angle2,angle3){
     const sumOfAngles = angle1 + angle2 + angle3;
-    console.log(sumOfAngles);
+    
     return sumOfAngles;
 }
 
 function isTriangle(){
      sumOfAngles = calculateSum(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value))
-    if(sumOfAngles===180){
-       // console.log("Yes")
+   
+   
+     if(sumOfAngles===180){
+       
        output.innerText = "Hurrah! It forms a triangle"
-    }
+    } else if(inputs[0].value < 0){
+      output.innerText = "Angle cannot be negative"
+    } else if(inputs[1].value < 0){
+      output.innerText = "Angle cannot be negative"
+    } else if(inputs[2].value < 0){
+      output.innerText = "Angle cannot be negative"
+    } 
+    else if(sumOfAngles > 180){
+       output.innerText = "Angle cannot be greater than 180"
+    } else if(inputs[0].value === ""){
+       output.innerText = "Fill the angle box so we can check"
+    } else if(inputs[1].value === ""){
+      output.innerText = "Fill the angle box so we can check"
+   } else if(inputs[2].value === ""){
+      output.innerText = "Fill the angle box so we can check"
+   }
+    
     else{
-       // console.log("No")
+       
        output.innerText = "Nope,try again"
     }
 }
